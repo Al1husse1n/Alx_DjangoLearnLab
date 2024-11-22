3af1460c0512a5bdf962821ecd05fb70e522e849
Ali Hussein <ah0979075074@gmail.com>
2024-11-22 21:48:24 +0300

    edited

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -7,7 +7,7 @@ specific_author = Author.objects.get(name='Author Name')
 books_by_author = Book.objects.filter(author=specific_author)
 print("Books by Author:", books_by_author)
 
-specific_library = Library.objects.get(name='library Name')
+specific_library = Library.objects.get(name='library_name')
 books_in_library = specific_library.books.all()
 print("Books in Library:", books_in_library)
 
